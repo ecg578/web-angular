0688da79b1a678fdc8bd5fb01392420215578285
ecg578 <ecg578@inlumine.ual.es>
2024-03-01 19:27:10 +0100

    Subida de componentes

--- web-nutricion/src/app/app.component.ts
+++ web-nutricion/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { IndexComponent } from './index/index.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { PlanesComponent } from './planes/planes.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent, IndexComponent, ContactoComponent, PlanesComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- web-nutricion/src/app/app.routes.ts
+++ web-nutricion/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { IndexComponent } from './index/index.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { PlanesComponent } from './planes/planes.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: '', component: IndexComponent},
+    {path: 'planes', component: PlanesComponent},
+    {path: 'contacto', component: ContactoComponent},
+    
+];
